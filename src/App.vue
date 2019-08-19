df6c0437e75a7b9c0552a4db65ee094f3ad9d6fa
Wcecilia <2466782395@qq.com>
2019-08-19 15:33:44 +0800

    add file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <main-tab></main-tab>
+  </div>
+</template>
+<script>
+import MainTab from '@/components/tabBottom/mainTab'
+export default {
+  components: {
+    MainTab
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
---
+++ src/views/Classify.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="classify">
+         <h2>我是分类</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
